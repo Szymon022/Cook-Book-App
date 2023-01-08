4f1b1644d0ea559d382b7adb8f10325a933b0ac6
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-08 12:20:21 +0100

    Add RouterCubit tests

---
+++ test/navigation/router_cubit_test.dart
@@ -0,0 +1,51 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:cook_book_app/navigation/router_cubit.dart';
+import 'package:cook_book_app/navigation/router_state.dart';
+import 'package:cook_book_app/recipe/recipe.dart';
+
+class MockRouterCubit extends MockCubit<RouterState> implements RouterCubit {}
+
+void main() {
+  Recipe recipe = const Recipe('Recipe 1');
+
+  blocTest<RouterCubit, RouterState>(
+    'emits [] on create',
+    build: () => RouterCubit(),
+    expect: () => [],
+  );
+
+  blocTest<RouterCubit, RouterState>(
+    'emits [HomePageState] when navigateToHome() is called',
+    build: () => RouterCubit(),
+    act: (cubit) => cubit.navigateToHome(),
+    expect: () => [const HomePageState()],
+  );
+
+  blocTest<RouterCubit, RouterState>(
+    'emits [EditRecipePageState] when navigateToEditRecipe() is called',
+    build: () => RouterCubit(),
+    act: (cubit) => cubit.navigateToEditRecipe(),
+    expect: () => [const EditRecipePageState()],
+  );
+
+  blocTest<RouterCubit, RouterState>(
+    'emits [EditRecipePageState] with recipe when navigateToEditRecipe() is called with given recipe',
+    build: () => RouterCubit(),
+    act: (cubit) => cubit.navigateToEditRecipe(recipe),
+    expect: () => [EditRecipePageState(recipe)],
+  );
+
+  blocTest<RouterCubit, RouterState>(
+    'emits [RecipePageState] with proper recipe when navigateToRecipe() is called',
+    build: () => RouterCubit(),
+    act: (cubit) => cubit.navigateToRecipe(recipe),
+    expect: () => [RecipePageState(recipe)],
+  );
+
+  blocTest<RouterCubit, RouterState>(
+    'emits [HomePageState] when popExtra() is called',
+    build: () => RouterCubit(),
+    act: (cubit) => cubit.popExtra(),
+    expect: () => [const HomePageState()],
+  );
+}
