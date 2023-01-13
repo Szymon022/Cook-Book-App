672ede7ce4e0cd569c2281be5b951e0c4d8434c6
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-13 18:38:26 +0100

    Add MultiBlocProvider

--- lib/app_root.dart
+++ lib/app_root.dart
@@ -1,3 +1,4 @@
+import 'package:cook_book_app/home/bloc/home_page_cubit.dart';
 import 'package:cook_book_app/navigation/router_cubit.dart';
 import 'package:cook_book_app/navigation/router_state.dart';
 import 'package:flutter/material.dart';
@@ -11,8 +12,10 @@ class AppRoot extends StatelessWidget {
   AppRoot({super.key});
 
   @override
-  Widget build(BuildContext context) => BlocProvider(
-        create: (context) => RouterCubit(),
+  Widget build(BuildContext context) => MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => RouterCubit()),
+        ],
         child: MaterialApp(home: _router),
       );
 
