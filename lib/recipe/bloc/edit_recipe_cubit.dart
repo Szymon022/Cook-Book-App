b08cde8e18d9d85a32c24c7cfa9b3eb1f5cc2b84
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-18 21:59:39 +0100

    Add Recipe saving on saveRecipe

--- lib/recipe/bloc/edit_recipe_cubit.dart
+++ lib/recipe/bloc/edit_recipe_cubit.dart
@@ -1,18 +1,23 @@
 import 'package:cook_book_app/navigation/router_cubit.dart';
+import 'package:cook_book_app/storage/recipe_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../storage/entity/recipe.dart';
 import 'edit_recipe_view_state.dart';
 
 class EditRecipeCubit extends Cubit<EditRecipeViewState> {
-  EditRecipeCubit(RouterCubit routerCubit, Recipe? recipe)
+  EditRecipeCubit(RouterCubit routerCubit, RecipeRepository recipeRepository,
+      Recipe? recipe)
       : _routerCubit = routerCubit,
+        _recipeRepository = recipeRepository,
         super(EditRecipeViewState(recipe));
 
   final RouterCubit _routerCubit;
+  final RecipeRepository _recipeRepository;
 
   void saveRecipe(Recipe recipe) {
     // save recipe here
+    _recipeRepository.saveRecipe(recipe);
     _routerCubit.popExtra();
   }
 
