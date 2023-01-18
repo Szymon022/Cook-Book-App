6a4360521d4e66d397cbc1adf05bb9c9627ea78d
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-18 18:55:11 +0100

    Add CookBookHive init on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:cook_book_app/app_root.dart';
+import 'package:cook_book_app/storage/cook_book_hive.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await CookBookHive.init();
   runApp(const MyApp());
 }
 
