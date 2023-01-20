01cc95f3f9545a589a16731a23b5cf6c334fe9a9
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-20 21:03:48 +0100

    Update edit_recipe_cubit_test.dart

--- test/recipe/bloc/edit_recipe_cubit_test.dart
+++ test/recipe/bloc/edit_recipe_cubit_test.dart
@@ -4,13 +4,19 @@ import 'package:cook_book_app/storage/entity/recipe.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../mock/mock_router_cubit.dart';
+import '../../utils/stubs.dart';
 
 void main() {
-  Recipe recipe = const Recipe('Recipe 1', '10 min', '300 kcals');
+  Recipe recipe = StubRecipe();
   MockRouterCubit routerCubit = MockRouterCubit();
+  MockRecipeRepository recipeRepository = MockRecipeRepository();
 
   blocTest('calls RouterCubit popExtra on goBack method',
-      build: () => EditRecipeCubit(routerCubit, recipe),
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.goBack(),
       expect: () => [],
       verify: (_) {
@@ -18,10 +24,51 @@ void main() {
       });
 
   blocTest('calls RouterCubit popExtra on saveRecipe method',
-      build: () => EditRecipeCubit(routerCubit, recipe),
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.saveRecipe(recipe),
       expect: () => [],
       verify: (_) {
         verify(() => routerCubit.popExtra()).called(1);
+      });
+
+  blocTest('calls RecipeRepository saveRecipe on saveRecipe method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.saveRecipe(recipe),
+      expect: () => [],
+      verify: (_) {
+        verify(() => recipeRepository.saveRecipe(recipe)).called(1);
+      });
+
+  blocTest('calls RouterCubit popExtra on updateRecipe method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.updateRecipe(recipe),
+      expect: () => [],
+      verify: (_) {
+        verify(() => routerCubit.popExtra()).called(1);
+      });
+
+  blocTest('calls RecipeRepository updateRecipe on updateRecipe method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.updateRecipe(recipe),
+      expect: () => [],
+      verify: (_) {
+        verify(() => recipeRepository.updateRecipe(recipe.uuid, recipe))
+            .called(1);
       });
 }
