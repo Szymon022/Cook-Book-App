7a145a374ca0ad9f76e6ca4b5a8ff7a816ed5e49
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-21 09:52:24 +0100

    Update edit_recipe_cubit_test.dart

--- test/recipe/bloc/edit_recipe_cubit_test.dart
+++ test/recipe/bloc/edit_recipe_cubit_test.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:cook_book_app/recipe/bloc/edit_recipe_cubit.dart';
+import 'package:cook_book_app/recipe/bloc/edit_recipe_view_state.dart';
 import 'package:cook_book_app/storage/entity/recipe.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -18,10 +19,18 @@ void main() {
       },
       build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.goBack(),
-      expect: () => [],
       verify: (_) {
         verify(() => routerCubit.popExtra()).called(1);
       });
+
+  blocTest('emits ShouldNotShowCamera on goBack method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.goBack(),
+      expect: () => [ShouldNotShowCamera()]);
 
   blocTest('calls RouterCubit popExtra on saveRecipe method',
       setUp: () {
@@ -30,7 +39,6 @@ void main() {
       },
       build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.saveRecipe(recipe),
-      expect: () => [],
       verify: (_) {
         verify(() => routerCubit.popExtra()).called(1);
       });
@@ -42,10 +50,18 @@ void main() {
       },
       build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.saveRecipe(recipe),
-      expect: () => [],
       verify: (_) {
         verify(() => recipeRepository.saveRecipe(recipe)).called(1);
       });
+
+  blocTest('emits ShouldNotShowCamera on saveRecipe method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.saveRecipe(recipe),
+      expect: () => [ShouldNotShowCamera()]);
 
   blocTest('calls RouterCubit popExtra on updateRecipe method',
       setUp: () {
@@ -54,7 +70,6 @@ void main() {
       },
       build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.updateRecipe(recipe),
-      expect: () => [],
       verify: (_) {
         verify(() => routerCubit.popExtra()).called(1);
       });
@@ -66,9 +81,26 @@ void main() {
       },
       build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
       act: (cubit) => cubit.updateRecipe(recipe),
-      expect: () => [],
       verify: (_) {
         verify(() => recipeRepository.updateRecipe(recipe.uuid, recipe))
             .called(1);
       });
+
+  blocTest('emits ShouldNotShowCamera on saveRecipe method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.saveRecipe(recipe),
+      expect: () => [ShouldNotShowCamera()]);
+
+  blocTest('emits ShouldShowCamera on onTakingPictureStarted method',
+      setUp: () {
+        clearInteractions(routerCubit);
+        clearInteractions(recipeRepository);
+      },
+      build: () => EditRecipeCubit(routerCubit, recipeRepository, recipe),
+      act: (cubit) => cubit.onTakingPictureStarted(),
+      expect: () => [ShouldShowCamera()]);
 }
